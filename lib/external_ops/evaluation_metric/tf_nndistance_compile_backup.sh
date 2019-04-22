246d94b0c3ba82496acf28cf80514df09fe97c92
weiyi <295902947@qq.com>
2019-04-23 00:03:01 +0800

    polish

--- lib/external_ops/evaluation_metric/tf_nndistance_compile_backup.sh
+++
@@ -1,2 +0,0 @@
-TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-/usr/local/cuda-8.0/bin/nvcc -std=c++11 -c -o tf_nndistance_g.cu.o tf_nndistance_g.cu -I ~/.local/lib/python2.7/site-packages/tensorflow/include -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -O2  -ltensorflow_framework && g++ -std=c++11 tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -shared -fPIC -I ~/.local/lib/python2.7/site-packages/tensorflow/include -L /usr/local/cuda-8.0/lib64 -O2 -ltensorflow_framework
