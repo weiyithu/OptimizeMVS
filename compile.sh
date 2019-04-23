704cfc06dbb2d99bfd5b5657a30e3423e5b6bf99
weiyi <295902947@qq.com>
2019-04-23 11:40:20 +0800

    polish compile.sh

--- compile.sh
+++ compile.sh
@@ -1,10 +1,15 @@
+#cuda and python path
+cuda_path=/usr/local/cuda-8.0
+py_path=~/.local/lib/python2.7
+
 # build evaluation metric
 cd lib/external_ops/evaluation_metric
-sh tf_approxmatch_compile.sh
-sh tf_nndistance_compile.sh
+sh tf_approxmatch_compile.sh $cuda_path $py_path
+sh tf_nndistance_compile.sh $cuda_path $py_path
 
 # build farthest sampling
 cd ../farthest_sampling
-sh tf_sampling_compile.sh
+sh tf_sampling_compile.sh $cuda_path $py_path
 cd ../../..
+
 
--- lib/external_ops/evaluation_metric/tf_approxmatch_compile.sh
+++ lib/external_ops/evaluation_metric/tf_approxmatch_compile.sh
@@ -1,10 +1,10 @@
 set -e
 if [ 'tf_approxmatch_g.cu.o' -ot 'tf_approxmatch_g.cu' ] ; then
 	echo 'nvcc'
-	/usr/local/cuda-8.0/bin/nvcc tf_approxmatch_g.cu -o tf_approxmatch_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+	${1}"/bin/nvcc" tf_approxmatch_g.cu -o tf_approxmatch_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 fi
 if [ 'tf_approxmatch_so.so'  -ot 'tf_approxmatch.cpp' ] || [ 'tf_approxmatch_so.so'  -ot 'tf_approxmatch_g.cu.o' ] ; then
 	echo 'g++'
-	g++ -std=c++11 tf_approxmatch.cpp tf_approxmatch_g.cu.o -o tf_approxmatch_so.so -shared -fPIC -I ~/.local/lib/python2.7/site-packages/tensorflow/include -I /usr/local/cuda-8.0/include  -L /usr/local/cuda-8.0/lib64/ -O2
+	g++ -std=c++11 tf_approxmatch.cpp tf_approxmatch_g.cu.o -o tf_approxmatch_so.so -shared -fPIC -I ${2}"/site-packages/tensorflow/include" -I ${1}"/include"  -L ${1}"/lib64/" -O2
 fi
 
